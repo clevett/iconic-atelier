ea33be6f26b29e745f11aa70f1e9839483d50a13
CassieLevett <cassielevett@gmail.com>
2024-10-11 13:26:29 +0000

    add studio picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-import { Heading, Flex, Text, RevealFx } from "@/once-ui/components";
+import {
+  Heading,
+  Flex,
+  Text,
+  RevealFx,
+  SmartImage,
+} from "@/once-ui/components";
 
-import { baseURL, home } from "@/app/resources";
+import { baseURL, home, tattoos } from "@/app/resources";
 import { Socials } from "@/app/components";
 
 export default function Home() {
@@ -52,18 +58,19 @@ export default function Home() {
               {home.subline}
             </Text>
           </RevealFx>
-          <RevealFx translateY="12" delay={0.4}>
-            <Text
-              wrap="balance"
-              onBackground="neutral-weak"
-              variant="body-default-l"
-            >
-              {home.address}
-            </Text>
-          </RevealFx>
         </Flex>
         <RevealFx translateY="12" delay={0.5}>
           <Socials />
+        </RevealFx>
+        <RevealFx translateY="16" delay={0.8}>
+          <SmartImage
+            alt="Iconic Atelier is modern and stylish art studio"
+            aspectRatio="16 / 9"
+            priority={true}
+            radius="m"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            src="/images/paintings/image (21).jpg"
+          />
         </RevealFx>
       </Flex>
     </Flex>
