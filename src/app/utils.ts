d80925ac9e43791ae3c971e3b4b2fa49db7eab0e
CassieLevett <cassielevett@gmail.com>
2024-10-17 10:24:25 +0000

    save wip

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -102,31 +102,3 @@ export function formatDate(date: string, includeRelative = false) {
 
   return `${fullDate} (${formattedDate})`;
 }
-
-export function generateMetadata(gallery: Gallery) {
-  const { title, description, label } = gallery;
-  const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
-
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      type: "website",
-      url: `https://${baseURL}/${label.toLowerCase()}`,
-      images: [
-        {
-          url: ogImage,
-          alt: title,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      images: [ogImage],
-    },
-  };
-}
