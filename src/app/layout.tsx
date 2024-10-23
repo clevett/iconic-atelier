13ea5ce33ca18e72ac5bf96a43e8813df8a63784
CassieLevett <cassielevett@gmail.com>
2024-10-23 13:13:48 +0000

    try import fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,32 +38,27 @@ export const metadata: Metadata = {
     },
   },
 };
-const path = "../app/fonts";
-const myFont = { className: "" };
-// const myFont = localFont({
-//   src: [
-//     {
-//       path: path + "/eina-01-regular.woff2",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     {
-//       path: path + "/eina-01-light.woff2",
-//       weight: "400",
-//       style: "light",
-//     },
-//     {
-//       path: path + "/eina-01-bold.woff2",
-//       weight: "700",
-//       style: "bold",
-//     },
-//     {
-//       path: path + "/eina-01-semibold.woff2",
-//       weight: "500",
-//       style: "semi-bold",
-//     },
-//   ],
-// });
+
+const myFont = localFont({
+  variable: "--font-eina",
+  src: [
+    {
+      path: "eina-03-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "eina-03-bold.woff2",
+      weight: "700",
+      style: "bold",
+    },
+    {
+      path: "eina-03-semibold.woff2",
+      weight: "500",
+      style: "semi-bold",
+    },
+  ],
+});
 
 const primary = Inter({
   variable: "--font-primary",
